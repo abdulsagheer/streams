9a60992e7c55cdc4489ebe12fb26a4b10ff17cf6
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-12 19:41:36 +0530

    Added router to A pp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+  return <div>PageTwo</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" exact element={PageOne} />
+        <Route path="/pagetwo" element={PageTwo} />
+      </Routes>
     </div>
   );
 }
